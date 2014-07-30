c4b9ad2e2fb76ad09d2bc26e74dbc75167ebf944
bashnesnos <bashnesnos@gmail.com>
2014-07-30 14:27:06 +0400

    Works enough to see linear difference

--- cache/CacheFun.java
+++ cache/CacheFun.java
@@ -1,3 +1,5 @@
+import static java.lang.System.out;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -6,8 +8,11 @@ import java.util.LinkedList;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
-
-import static java.lang.System.out;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 public class CacheFun {
 
@@ -21,9 +26,11 @@ public class CacheFun {
 	
 	private static LinkedFIFOCache<String, Object> linkedFIFO;
 	private static LinkedLRUCache<String, Object> linkedLRU;
-	
+
+        private static boolean printKeys = false;
+        
 	public static void main(String[] args) {
-		int size = 33;
+		int size = 300000;
 		
 		if (args != null && args.length == 1) {
 			try {
@@ -35,9 +42,9 @@ public class CacheFun {
 		}
 		
 		
-		linkedFIFO = new LinkedFIFOCache<String, Object>(size);
+                linkedFIFO = new LinkedFIFOCache<String, Object>(1000);
 
-		//keySet iteration matters
+		//keySet iteration order matters
 		for (int i = 0; i < size; i++) {
 			linkedFIFO.put(String.format(KEY_FORMAT, i), new Object());
 		}
@@ -45,19 +52,72 @@ public class CacheFun {
 		naiveFIFO = new NaiveFIFOCache<String, Object>(linkedFIFO);
 		naiveLRU =  new NaiveLRUCache<String, Object>(linkedFIFO);
 		linkedLRU = new LinkedLRUCache<String, Object>(linkedFIFO);
-
-		
+                
 		checkNoGet();
 		checkWithGet();
 		checkRotation();
 		checkRotationGets();
 		
 		out.println("\nSmoke test successfull");
+                
+                linkedFIFO = new LinkedFIFOCache<String, Object>(size);
+
+		//keySet iteration order matters
+		for (int i = 0; i < size; i++) {
+			linkedFIFO.put(String.format(KEY_FORMAT, i), new Object());
+		}
+		
+		naiveFIFO = new NaiveFIFOCache<String, Object>(linkedFIFO);
+                if (size > 30000) { //otherwise too slow
+                    naiveLRU = new NaiveLRUCache<String, Object>(30000);
+                    naiveLRU.putAll(linkedFIFO);
+                }
+                else {
+                    naiveLRU =  new NaiveLRUCache<String, Object>(linkedFIFO);
+                }
+		linkedLRU = new LinkedLRUCache<String, Object>(linkedFIFO);
+                
+                ExecutorService loadThreadPool = Executors.newFixedThreadPool(4);
+                
+                int iterationsNum = 100000;
+               
+                if (args != null && args.length == 2) {
+			try {
+                            iterationsNum = Integer.valueOf(args[1]);
+			}
+			catch (NumberFormatException ex) {
+                            out.println("Wrong number: " + args[1]);
+			}
+		}
+                
+                out.println("\nMap size: " + size + "; number of iterations: " + iterationsNum);
+                
+                Future<Long> naiveFIFOFuture = loadThreadPool.submit(new MoreGetsCacheLoadCheckCallable(naiveFIFO, iterationsNum));                
+                Future<Long> naiveLRUFuture = loadThreadPool.submit(new MoreGetsCacheLoadCheckCallable(naiveLRU, Math.min(100,iterationsNum)));
+                Future<Long> linkedFIFOFuture = loadThreadPool.submit(new MoreGetsCacheLoadCheckCallable(linkedFIFO, iterationsNum));
+                Future<Long> linkedLRUFuture = loadThreadPool.submit(new MoreGetsCacheLoadCheckCallable(linkedLRU, iterationsNum));
+                
+                try {
+
+                    out.println("1. Linked FIFO millis: " + linkedFIFOFuture.get());
+                    out.println("2. Naive FIFO millis: " + naiveFIFOFuture.get()); 
+                    out.println("3. Linked LRU millis: " + linkedLRUFuture.get());
+                    out.println("Naive LRU " + (size <= 30000 ? "" : "(size reduced to 30K) ") + (iterationsNum <= 100 ? "" : "(iter num reduced to 100)") + " millis: " + naiveLRUFuture.get()); //I bet it's the slowest
+                    
+                    loadThreadPool.shutdown();
+                    loadThreadPool.awaitTermination(60, TimeUnit.SECONDS);
+                } catch (Exception ex) {
+                    throw new RuntimeException(ex);
+                }
+                finally {
+                    loadThreadPool.shutdownNow();
+                }
+   
 	}
 
 	static void checkNoGet() {
 		String extraKey = "noGetKey";
-		out.println("\nNo Get");
+		out.print("Testing put without gets: ");
 		
 		naiveFIFO.put(extraKey, new Object());
 		naiveLRU.put(extraKey, new Object());
@@ -66,14 +126,15 @@ public class CacheFun {
 
 		checkConsistency();
 		
+                out.println(" V");
 	}
 
 	static void checkWithGet() {
 		String extraKey = "withGetKey";
-		out.println("\nWith Get");
+		out.print("Testing put after gets: ");
 		
-		for (int i = 0; i < naiveFIFO.size(); i++) {
-			String key = String.format(KEY_FORMAT, i);
+		for (int i = 0; i < Math.min(1000, naiveFIFO.size()); i++) {
+			String key = String.format("new_key_%d", i);
 			naiveFIFO.get(key);
 			naiveLRU.get(key);
 			linkedFIFO.get(key);
@@ -86,13 +147,14 @@ public class CacheFun {
 		linkedLRU.put(extraKey, new Object());
 
 		checkConsistency();
-				
+		
+                out.println(" V");
 	}	
 
 	static void checkRotation() {
-		out.println("\nRotation");
+		out.print("Testing rotation: ");
 		
-		for (int i = 0; i < naiveFIFO.size()*3; i++) {
+		for (int i = 0; i < Math.min(1000, naiveFIFO.size())*2; i++) {
 			String key = String.format("new_key_%d", i);
 			Object val = new Object();
 			naiveFIFO.put(key, val);
@@ -102,26 +164,28 @@ public class CacheFun {
 		}
 
 		checkConsistency();
-				
+		
+                out.println(" V");
+                
 	}
 	
 	
 	static void checkRotationGets() {
-		out.println("\nRotation and gets");
+		out.print("Testing rotation with intermittent gets: ");
 		
 		String pickedKey = linkedLRU.keySet().iterator().next();
 		
-		for (int i = 0; i < naiveFIFO.size()*17; i++) {
-			String key = String.format("new_key_%d", i);
+		for (int i = 0; i < Math.min(1000, naiveFIFO.size())*2; i++) {
+			String key = String.format(KEY_FORMAT, i);
 			Object val = new Object();
 			naiveFIFO.get(pickedKey);
 			naiveFIFO.put(key, val);
-			naiveLRU.get(pickedKey);
-			naiveLRU.put(key, val);
+                        naiveLRU.get(pickedKey);
+                        naiveLRU.put(key, val);
+                        linkedLRU.get(pickedKey);
+                        linkedLRU.put(key, val);
 			linkedFIFO.get(pickedKey);
 			linkedFIFO.put(key, val);
-			linkedLRU.get(pickedKey);
-			linkedLRU.put(key, val);
 		}
 
 		checkConsistency();
@@ -133,6 +197,7 @@ public class CacheFun {
 		assertTrue("Linked LRU should contain frequently used key", linkedLRU.get(pickedKey) != null);
 		assertTrue("Naive LRU should contain frequently used key", naiveLRU.get(pickedKey) != null);
 				
+                out.println(" V");
 	}
 	
 	static void checkConsistency() {
@@ -146,11 +211,13 @@ public class CacheFun {
 	}
 	
 	static void printMapsKeySet(String type, Map<?, ?> linked , Map<?, ?> naive) {
+             if (printKeys) {
 		out.println("\n" + type);
 		out.println("Naive");
 		out.println(naive.keySet());
 		out.println("Linked");
 		out.println(linked.keySet());
+             }
 	}
 	
 	static void assertSizeAndKeysMatch(Map<?, ?> linked, Map<?, ?> naive) {
@@ -173,12 +240,12 @@ abstract class NaiveAbstractLinkedFixedSizeHashMap<K, V> extends HashMap<K, V> {
 	protected final LinkedList<K> keyQueue = new LinkedList<K>();
 	
 	public NaiveAbstractLinkedFixedSizeHashMap(int size) {
-		super(size, 1.0f);
+		super(size + 3, 1.0f);
 		maxCapacity = size;
 	}
 	
 	public NaiveAbstractLinkedFixedSizeHashMap(Map<? extends K, ? extends V> sourceMap) {
-		super(sourceMap.size(), 1.0f);
+		super(sourceMap.size() + 3, 1.0f);
 		for (Map.Entry<? extends K, ? extends V> newEntry : sourceMap.entrySet()) {
 			K nextKey = newEntry.getKey();
 			markNew(nextKey);
@@ -189,11 +256,18 @@ abstract class NaiveAbstractLinkedFixedSizeHashMap<K, V> extends HashMap<K, V> {
 	
 	@Override
 	public V put(K key, V val) {
-		if (size() == maxCapacity) {
+                boolean existingKey = containsKey(key); //depends on hashCode & equals
+                V result = super.put(key, val);
+		if (size() > maxCapacity) {
 			removeOld();
 		}
-		markNew(key);
-		return super.put(key, val);
+                if (existingKey) {
+                    updateKey(key);
+                }
+                else {
+                    markNew(key);
+                }
+		return result;
 	}
 
 	@Override
@@ -229,6 +303,10 @@ abstract class NaiveAbstractLinkedFixedSizeHashMap<K, V> extends HashMap<K, V> {
 	protected void markNew(K key) {
 		keyQueue.add(key);
 	}
+        
+        protected void updateKey(K key) {
+		
+	}
 }
 
 class NaiveFIFOCache<K, V> extends NaiveAbstractLinkedFixedSizeHashMap<K, V> {
@@ -260,14 +338,22 @@ class NaiveLRUCache<K, V> extends NaiveAbstractLinkedFixedSizeHashMap<K, V> {
 	@SuppressWarnings("unchecked")
 	@Override
 	public V get(Object key) {
-		int idxToMakeLast = keyQueue.indexOf(key);
-		if (idxToMakeLast != -1) {
-			keyQueue.remove(idxToMakeLast);			
-			keyQueue.add((K) key); //passed indexOf equals check; bad K.equals might cause ClassCastException, so it's dirty
-		}
-		return super.get(key);
+            V result = super.get(key);
+            if (result != null) {
+                updateKey((K) key); //passed get equals check
+            }
+            return result;
 	}
-	
+
+        @Override
+        protected void updateKey(K key) { //source of troubles
+            int idxToMakeLast = keyQueue.indexOf(key); //O(n)
+            if (idxToMakeLast != -1) {
+                    keyQueue.remove(idxToMakeLast); //O(n)			
+                    keyQueue.add(key);
+            }
+	}
+        
 }
 
 class LinkedFIFOCache<K, V> extends LinkedHashMap<K, V> {
@@ -275,13 +361,14 @@ class LinkedFIFOCache<K, V> extends LinkedHashMap<K, V> {
 	private final int maxCapacity;
 	
 	public LinkedFIFOCache(int size) {
-		super(size, 1.0f);
+		super(size + 3, 1.0f);
 		maxCapacity = size;
 	}
 	
 	public LinkedFIFOCache(Map<? extends K, ? extends V> sourceMap) {
-		super(sourceMap);
+		super(sourceMap.size() + 3, 1.0f, true);
 		maxCapacity = sourceMap.size();
+		putAll(sourceMap);
 	}		
 
 	@Override
@@ -296,12 +383,12 @@ class LinkedLRUCache<K, V> extends LinkedHashMap<K, V> {
 	private final int maxCapacity;
 	
 	public LinkedLRUCache(int size) {
-		super(size, 1.0f, true);
+		super(size + 3 , 1.0f, true);
 		maxCapacity = size;
 	}
 	
 	public LinkedLRUCache(Map<? extends K, ? extends V> sourceMap) {
-		super(sourceMap.size(), 1.0f, true);
+		super(sourceMap.size() + 3, 1.0f, true);
 		maxCapacity = sourceMap.size();
 		putAll(sourceMap);
 	}		
@@ -311,4 +398,108 @@ class LinkedLRUCache<K, V> extends LinkedHashMap<K, V> {
 		return size() > maxCapacity;
 	}
 	
+}
+
+class CacheLoadCheckCallable implements Callable<Long> {
+
+    protected final Map<String, Object> testMap;
+    protected final int opsPerIterCount;
+    protected int iterations;
+    protected int keyIdxOffset;
+
+    public CacheLoadCheckCallable(Map<String, Object> testMap, int iterations) {
+        this.iterations = iterations;
+        this.testMap = testMap;
+        this.keyIdxOffset = testMap.size();
+        this.opsPerIterCount = keyIdxOffset < 10 ? 1 :  Math.round(keyIdxOffset * (keyIdxOffset < 10000 ? 0.1f : 0.001f));
+    }
+
+    @Override
+    public Long call() throws Exception {
+
+        long start = new Date().getTime();
+        while(iterations > 0) {
+            Iterator<String> keyIter = testMap.keySet().iterator();
+            String[] opKeys = new String[opsPerIterCount];
+            for (int i = 0; i < opsPerIterCount; i++) {
+                if (keyIter.hasNext()) {
+                    opKeys[i] = keyIter.next();
+                }
+                else if (i > 0) {
+                    opKeys[i] = opKeys[i - 1];
+                }
+                else {
+                    opKeys[i] = "dummy";
+                }
+            }
+
+            for (int i = 0; i < opsPerIterCount; i++) {
+                if (i % 2 == 0) {
+                    testMap.get(opKeys[i]);
+                }
+                else if (i % 3 == 0) {
+                    testMap.get(opKeys[i]);
+                    testMap.put(String.format(CacheFun.KEY_FORMAT, keyIdxOffset + i), new Object());
+                }
+                else {
+                    testMap.put(String.format(CacheFun.KEY_FORMAT, keyIdxOffset + i), new Object());
+                }
+            }
+            keyIdxOffset += opsPerIterCount;
+            iterations--;
+        }
+        return new Date().getTime() - start;
+    }
+
+}
+
+class MoreGetsCacheLoadCheckCallable extends CacheLoadCheckCallable {
+
+
+    public MoreGetsCacheLoadCheckCallable(Map<String, Object> testMap, int iterations) {
+        super(testMap, iterations);
+    }
+
+    @Override
+    public Long call() throws Exception {
+
+        long start = new Date().getTime();
+        while(iterations > 0) {
+            Iterator<String> keyIter = testMap.keySet().iterator();
+            String[] opKeys = new String[opsPerIterCount];
+            for (int i = 0; i < opsPerIterCount; i++) {
+                if (keyIter.hasNext()) {
+                    opKeys[i] = keyIter.next();
+                }
+                else if (i > 0) {
+                    opKeys[i] = opKeys[i - 1];
+                }
+                else {
+                    opKeys[i] = "dummy";
+                }
+            }
+
+            for (int i = 0; i < opsPerIterCount; i++) {
+                if (i % 2 == 0) {
+                    testMap.get(opKeys[i]);
+                    testMap.get(opKeys[opsPerIterCount - i - 1]);
+                    testMap.get(opKeys[opsPerIterCount - i - 1]);
+                }
+                else if (i % 3 == 0) {
+                    testMap.get(opKeys[i]);
+                    testMap.get(opKeys[opsPerIterCount - i - 1]);
+                    testMap.get(opKeys[i]);
+                    testMap.get(opKeys[opsPerIterCount - i - 1]);
+                    testMap.put(String.format(CacheFun.KEY_FORMAT, keyIdxOffset + i), new Object());
+                }
+                else {
+                    testMap.put(String.format(CacheFun.KEY_FORMAT, keyIdxOffset + i), new Object());
+                }
+            }
+            keyIdxOffset += opsPerIterCount;
+            iterations--;
+        }
+        return new Date().getTime() - start;
+    }
+
 }
